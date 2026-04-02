c0781c984b87c98df2430a57d2fe3835e340d0d3
ritik rikhi <rikhiritik8@gmail.com>
2026-04-02 12:21:59 +0530

    fix: export syntax in api/contact.js

--- api/contact.js
+++ api/contact.js
@@ -34,7 +34,7 @@ async function connectToDatabase() {
   return cachedDb;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS setup just in case
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
